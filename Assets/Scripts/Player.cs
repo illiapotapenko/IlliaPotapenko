68eaa6084c79eca38a1a4bce8f1436d00e90d7b0
Illia <illy777potapenko@gmail.com>
2025-03-08 10:58:19 -0500

    New Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,32 +8,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
+    [SerializeField] private GameInput _gameInput;
 
     private bool isWalking;
     
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = + 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = _gameInput.GetMovementVector();
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * Time.deltaTime * moveSpeed;
 
