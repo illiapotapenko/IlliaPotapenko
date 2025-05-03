dc177f9a7d2e340ff4c1b261394f3bc2a17b3877
Illia <illy777potapenko@gmail.com>
2025-05-03 11:38:48 -0400

    Pick Up/Drop Objects + Add Kitchen Objects

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -10,6 +10,19 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent
 
   public override void Interact(Player player)
   {
+    if (!HasKitchenObject())
+    {
+      if (player.HasKitchenObject())
+      {
+        player.GetKitchenObject().SetKitchenObjectParent(this);
+      }
+    }else
+    {
+      if (!player.HasKitchenObject())
+      {
+        this.GetKitchenObject().SetKitchenObjectParent(player);
+      }
+    }
   }
 }
   
--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -11,9 +11,13 @@ public class ContainerCounter : BaseCounter, IKitchenObjectParent
     
     public override void Interact(Player player)
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        OnPlayerGrabedObject?.Invoke();
+        if (!player.HasKitchenObject())
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabedObject?.Invoke();
+        }
+        
     }
 }  
     
