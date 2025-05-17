356a964f060269ddc9f16165872cab8073924382
Illia <illy777potapenko@gmail.com>
2025-05-17 10:59:52 -0400

    Fix Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -98,7 +98,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         if (canMove == false)
         {
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
-            canMove =  !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirectionX,moveDistance);
+            canMove = moveDirection.x != 0 && !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirectionX,moveDistance);
 
             if (canMove)
             {
@@ -107,7 +107,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             else
             {
                 Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
-                canMove =  !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirectionZ,moveDistance);
+                canMove = moveDirection.z != 0 && !Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirectionZ,moveDistance);
 
                 if (canMove)
                 {
